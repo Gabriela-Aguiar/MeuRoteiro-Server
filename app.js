655322a96296509d572b49970dddedc20c652501
marcela <marcela_vieiraf@hotmail.com>
2020-05-22 10:28:03 -0300

    heroku

--- app.js
+++ app.js
@@ -69,7 +69,7 @@ app.locals.title = 'Express - Generated with IronGenerator';
 app.use(
   cors({
     credentials: true,
-    origin: ["http://localhost:3000", "http://meu-roteiro.herokuapp.com/"], // <== this will be the URL of our React app (it will be running on port 3000)
+    origin: ["http://localhost:3000", "http://meu-roteiro.herokuapp.com"], // <== this will be the URL of our React app (it will be running on port 3000)
   })
 );
 
