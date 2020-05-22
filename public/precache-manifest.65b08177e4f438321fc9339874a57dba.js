fc2c689768d0af5d805ebb28af9d433ca912f525
Gabriela Aguiar <aguiar.gabi95@gmail.com>
2020-05-21 22:29:06 -0300

    public added

---
+++ public/precache-manifest.65b08177e4f438321fc9339874a57dba.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eadee922a5292777d8748cfd731b20c1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f8abe03a1fbb59cbe127",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "eb7e2d4f0d629408bd9b",
+    "url": "/static/css/main.4c5778eb.chunk.css"
+  },
+  {
+    "revision": "f8abe03a1fbb59cbe127",
+    "url": "/static/js/2.0be05007.chunk.js"
+  },
+  {
+    "revision": "27b038be9f71ff3f552dd94662ce23e3",
+    "url": "/static/js/2.0be05007.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb7e2d4f0d629408bd9b",
+    "url": "/static/js/main.85a0b2f9.chunk.js"
+  },
+  {
+    "revision": "0e35831b56fb2c044e12",
+    "url": "/static/js/runtime-main.98a2b520.js"
+  }
+]);
